ca69750519d22217dfda09022759cd1eb1e39e64
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-09-07 10:54:23 +0530

    made signup

---
+++ main.js
@@ -0,0 +1,70 @@
+// variable declaration
+
+// username input is assigned to username
+const username = document.getElementById("username");
+
+// user entered password is assigned to password
+const password = document.getElementById("password");
+const wrapper = document.getElementById("wrapper");
+const logout = document.getElementById("logout");
+// user click on signup
+const signup = document.getElementById("signup");
+const signin = document.getElementById("signin");
+const p = document.getElementById("signmsg")
+
+
+// user entered input is stored array
+let data = [];
+
+
+// function to store the entered input user name and password
+function storesignup(){
+    let items = {
+                name: username.value,
+                pass: password.value
+                 };
+    let isUnique = true
+    data.forEach(
+        (e) => {
+            if(e.name === username.value){
+                isUnique = false;
+            }
+        }
+    )
+    if(isUnique){
+        data.push(items);  
+        p.innerText = "Sucessfully registred";       
+    } else {
+        p.innerText = "Already username is registered withus";
+    }
+}
+
+// function to check the signin
+function checksignin(){
+    data.forEach((e) => {
+                            if (e.name == username.value && e.pass == password.value) {
+                            wrapper.style.display = "none";
+                            logout.style.backgroundColor = "blue";
+                            logout.innerText = "logout";    
+                            }
+                            else{
+                                p.innerText = "Incorrect username or password"
+                            }
+                        }
+                    
+                )
+}
+
+function showsignupsignin(){
+    wrapper.style.display = "";
+    logout.style.display = "none";
+    username.value = "";
+    password.value = "";
+    p.innerText = "";
+
+}
+
+// adding event listners 
+signup.addEventListener("click", storesignup)
+signin.addEventListener("click", checksignin)
+logout.addEventListener("click",showsignupsignin)
